3314ae1d5a80a705bc83bcb79451b653b9e95bc6
Cory Shields <shieldscr88@gmail.com>
2016-08-14 13:26:53 -0400

    MainActivityTest created and Robolectric added

---
+++ app/src/test/java/com/dolphin/dolphinsms/MainActivityTest.java
@@ -0,0 +1,20 @@
+package com.dolphin.dolphinsms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class MainActivityTest {
+    @Test
+    public void mainActivityCanBeCreated() {
+        MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
+
+        assertNotNull(mainActivity);
+    }
+}
